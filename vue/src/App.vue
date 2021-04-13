86728d61ac9e92554b4c17e85a49c354f35ee6e8
a18wilis <49142833+a18wilis@users.noreply.github.com>
2021-04-13 22:00:16 +0200

    Add files via upload

---
+++ vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Heatmap/>
+  </div>
+</template>
+
+<script>
+import Heatmap from './components/Heatmap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Heatmap
+  }
+}
+</script>
+
+<style>
+#app {
+    width: 1000px;
+    height: 600px;
+    border: 1px solid #000000;
+    margin: auto;
+}
+</style>
